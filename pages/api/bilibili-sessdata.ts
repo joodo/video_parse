589b7618b97c5aa4d5a0038d00e5eb713ee9a283
lianghanzhong <wyattliang@gmail.com>
2023-06-11 14:49:29 +0800

    sessdatta

---
+++ pages/api/bilibili-sessdata.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    const { key } = req.query;
+
+    if (key !== process.env.BUNGA_KEY) {
+        res.status(400).json('Wrong key');
+    } else {
+        const sess = process.env.SESSDATA;
+        const response = await axios.get(
+            'https://passport.bilibili.com/x/passport-login/web/cookie/info',
+            { headers: { "Cookie": `SESSDATA=${sess}` } }
+        );
+        if (response.data.data.refresh) {
+            res.status(200).json('');
+        } else {
+            res.status(200).json(sess);
+        }
+    }
+
+}
